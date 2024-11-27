54209b9cd5c48f712f0351746e22bc0f526c2c64
Marcel Hellkamp <marc@gsites.de>
2024-11-27 15:37:48 +0100

    docs: Enable github banner and fixed sidebar

--- docs/conf.py
+++ docs/conf.py
@@ -56,6 +56,13 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 #html_theme = "sphinx_rtd_theme"
+html_theme_options = {
+    'github_user': 'defnull',
+    'github_repo': 'multipart',
+    'fixed_sidebar': True,
+    'github_banner': True,
+    'github_button': False,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
